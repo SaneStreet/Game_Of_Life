554bc190cbb1d77584a2a09771351f440fd59ef3
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-03-29 17:59:18 +0200

    Lavet GUI og en metode der tegner de døde og levende celler.

--- src/erik2310/Game.java
+++ src/erik2310/Game.java
@@ -1,11 +1,15 @@
 package erik2310;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+
 import java.util.Random;
 
 public class Game {
 
     // Definerer hvor stort vores array med celler skal være
-    private final int BOARD_SIZE_X = 10, BOARD_SIZE_Y = 10;
+    private final int BOARD_SIZE_X = 30, BOARD_SIZE_Y = 30;
 
     // 2 dimensionelt array med celler
     private Cell[][] cellsArray = new Cell[BOARD_SIZE_X][BOARD_SIZE_Y];
@@ -35,5 +39,35 @@ public class Game {
     public static Game createGame() {
         Game game = new Game();
         return game;
+    }
+
+    // Metode der løber arrayet igennem og tegner cellerne
+    public void drawCells() {
+        for (int y = 0, yPosition = 25; y < BOARD_SIZE_Y; y++, yPosition = yPosition + 22) {
+            for (int x = 0, xPosition = 25; x < BOARD_SIZE_X; x++, xPosition = xPosition + 22) {
+
+                if (cellsArray[x][y].isAlive()) {
+                    Rectangle rectangle = new Rectangle(20, 20, Color.BLUE);
+                    rectangle.setX(xPosition);
+                    rectangle.setY(yPosition);
+                    rectangle.setStrokeType(StrokeType.INSIDE);
+                    rectangle.setStroke(Color.BLACK);
+                    rectangle.setStrokeWidth(2);
+                    GameOfLifeGUI.getPane().getChildren().add(rectangle);
+                }
+
+                if (!cellsArray[x][y].isAlive()) {
+                    Rectangle rectangle = new Rectangle(20, 20, Color.WHITE);
+                    rectangle.setX(xPosition);
+                    rectangle.setY(yPosition);
+                    rectangle.setStrokeType(StrokeType.INSIDE);
+                    rectangle.setStroke(Color.BLACK);
+                    rectangle.setStrokeWidth(2);
+                    GameOfLifeGUI.getPane().getChildren().add(rectangle);
+                }
+
+            }
+        }
+
     }
 }
--- src/erik2310/GameOfLifeGUI.java
+++ src/erik2310/GameOfLifeGUI.java
@@ -7,17 +7,27 @@ import javafx.stage.Stage;
 
 public class GameOfLifeGUI extends Application {
 
+    // Laver et game objekt ved hjælp af Game.createGame metoden
+    private Game game = Game.createGame();
+
+    private static Pane pane = new Pane();
+
     @Override
     public void start(Stage primaryStage) {
-        Pane pane = new Pane();
-        Scene scene = new Scene(pane, 400, 400);
+        game.drawCells();
+        Scene scene = new Scene(pane, 700, 700);
         primaryStage.setTitle("Game Of Life");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.show();
+
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static Pane getPane() {
+        return pane;
     }
 }
